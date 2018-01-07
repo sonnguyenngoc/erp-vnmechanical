1d0bb5df4a1f80413419e1c9457b7f0f9bc78224
Hung Nguyen <hungnt@hoangkhang.com.vn>
2018-01-07 20:45:06 +0700

    update product list page

---
+++ app/controllers/erp/vnmechanical/frontend/category_controller.rb
@@ -0,0 +1,10 @@
+module Erp
+  module Vnmechanical
+    module Frontend
+      class CategoryController < Erp::Frontend::FrontendController
+        def index
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Erp::Vnmechanical::Engine.routes.draw do
   
   get "gioi-thieu.html" => "frontend/information#about_us", as: :about_us
   get "lien-he.html" => "frontend/information#contact_us", as: :contact_us
+  
+  get "san-pham.html" => "frontend/category#index", as: :product_listing
+  get "san-pham/chi-tiet.html" => "frontend/product#detail", as: :product_detail
 end
